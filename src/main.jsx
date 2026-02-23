85b5998b5b4e30736cb79b72bb9576cc555b9893
Luan <luan.soya@gmail.com>
2026-02-23 18:51:10 -0300

    Fix PWA configuration

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('✅ SW registrado'))
+      .then(reg => console.log('✅ SW registrado')) 
       .catch(err => console.log('❌ SW erro:', err))
   })
 }
